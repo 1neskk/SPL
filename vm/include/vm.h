cb812aa1baf2fd8d1139645d7a52d13c5094329c
1neskk <contato.nesk@outlook.com>
2025-05-05 21:50:20 -0300

    Refactor inline assembly inside `execute_instruction()` to use 32-bit registers
    - Add architecture macros to enhance compatibility across different architectures
    - If architecture is not x86 or x64 fallback to portable C code
    
    **TODO:** fix the `JMP` instruction since it's current implementation is incorrect.

--- vm/include/vm.h
+++ vm/include/vm.h
@@ -2,6 +2,7 @@
 #define VM_H
 
 #include <stdio.h>
+#include <stdlib.h>
 #include "instructions.h"
 
 #define NUM_REGISTERS 8
--- vm/src/instructions.c
+++ vm/src/instructions.c
@@ -1,5 +1,9 @@
 #include "vm.h"
 
+#if defined(__x86_64__) || defined(_M_X64) || defined(__i386__) || defined(_M_IX86)
+#define USE_ASM
+#endif
+
 void execute_instruction(VM* vm, Instruction instr)
 {
     int val1, val2, result;
@@ -20,7 +24,7 @@ void execute_instruction(VM* vm, Instruction instr)
             break;
         
         case OP_ADD:
-#if defined(__x86_64__) || defined(_M_X64)
+#ifdef USE_ASM
             asm volatile(
                 "add %[val2], %[val1]\n\t"
                 "mov %[val1], %[result]"
@@ -35,66 +39,98 @@ void execute_instruction(VM* vm, Instruction instr)
             break;
 
         case OP_SUB:
+#ifdef USE_ASM
             asm volatile(
                 "sub %[val2], %[val1]\n\t"
                 "mov %[val1], %[result]"
                 : [result] "=r" (result)
                 : [val1] "r" (val1), [val2] "r" (val2)
             );
+#else
+            result = val1 - val2;
+#endif
             set_operand_value(vm, instr.operands[0], result);
             vm->cpu.pc++;
             break;
 
         case OP_MUL:
+#ifdef USE_ASM
             asm volatile(
                 "imul %[val2], %[val1]\n\t"
                 "mov %[val1], %[result]"
                 : [result] "=r" (result)
                 : [val1] "r" (val1), [val2] "r" (val2)
             );
+#else
+            result = val1 * val2;
+#endif
             set_operand_value(vm, instr.operands[0], result);
             vm->cpu.pc++;
             break;
 
         case OP_DIV:
+            // TODO: Properly handle division by zero
             if (val2 == 0)
             {
                 fprintf(stderr, "Error: Division by zero\n");
                 exit(1);
             }
+
+#ifdef USE_ASM
             asm volatile(
-                "mov %[val1], %%rax\n\t"
-                "xor %%rdx, %%rdx\n\t"
+                "mov %[val1], %%eax\n\t"
+                "xor %%edx, %%edx\n\t"
                 "div %[val2]\n\t"
-                "mov %%rax, %[result]"
+                "mov %%eax, %[result]"
                 : [result] "=r" (result)
                 : [val1] "r" (val1), [val2] "r" (val2)
-                : "rax", "rdx"
+                : "eax", "edx"
             );
+#else
+            result = val1 / val2;
+#endif
             set_operand_value(vm, instr.operands[0], result);
             vm->cpu.pc++;
             break;
 
         case OP_CMP:
+#ifdef USE_ASM
             asm volatile(
-                "cmp %[val1], %[val2]\n\t"
-                "mov %[equal], %%rax\n\t"
-                "mov %[greater], %%rbx\n\t"
-                "mov %[less], %%rcx\n\t"
-                "cmove %%rax, %[flags]\n\t"
-                "cmovg %%rbx, %[flags]\n\t"
-                "cmovl %%rcx, %[flags]"
-                : [flags] "=r" (vm->cpu.flags)
+                "cmp %[val2], %[val1]\n\t"      // Compare val1 and val2
+                "mov %[equal], %%eax\n\t"       // Setup flag values
+                "mov %[greater], %%ebx\n\t"
+                "mov %[less], %%ecx\n\t"
+                "mov %%eax, %%edx\n\t"          // Store equal flag in result register
+                "cmovg %%ebx, %%edx\n\t"        // If greater, set result to greater flag
+                "cmovl %%ecx, %%edx\n\t"        // If less, set result to less flag
+                "mov %%edx, %[result]"          // Store result in flags
+                : [result] "=r" (vm->cpu.flags)
                 : [val1] "r" (val1), [val2] "r" (val2),
                   [equal] "i" (FLAG_EQUAL), [greater] "i" (FLAG_GREATER),
                   [less] "i" (FLAG_LESS)
-                : "cc", "rax", "rbx", "rcx"
+                : "cc", "eax", "ebx", "ecx", "edx"
             );
+#else
+            if (val1 == val2)
+                vm->cpu.flags = FLAG_EQUAL;
+            else if (val1 > val2)
+                vm->cpu.flags = FLAG_GREATER;
+            else
+                vm->cpu.flags = FLAG_LESS;
+#endif
             vm->cpu.pc++;
             break;
 
         case OP_JMP:
+#ifdef USE_ASM
+            asm volatile(
+                "jmp *%[addr]"
+                :
+                : [addr] "r" (find_label_address(vm, instr.operands[0].value.label))
+            );
+#else
             vm->cpu.pc = find_label_address(vm, instr.operands[0].value.label);
+#endif
             break;
     }
 }
