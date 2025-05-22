4874c057384ac05494ad6415b6f6ae13cf881bed
1neskk <contato.nesk@outlook.com>
2025-05-22 15:49:56 -0300

    Enhance memory management by implementing memory initialization and clearing functions

--- anvil/src/memory.s
+++ anvil/src/memory.s
@@ -1,20 +1,34 @@
 .intel_syntax noprefix
 
 .global memcopy_
-.type memcopy_, @function
+.global memclear_
 
-# rdi = dest, rsi = src, rdx = size
+.type memcopy_, @function
 memcopy_:
     test rdx, rdx   # Check if the size is zero
-    jz .done
+    jz .done_copy
 
-.loop:
+.loop_copy:
     mov bl, byte ptr [rsi]    # Move byte-by-byte from source to rdi (dest)
     mov byte ptr [rdi], bl
     inc rdi
     inc rsi
     dec rdx
-    jnz .loop
+    jnz .loop_copy
 
-.done:
+.done_copy:
+    ret
+
+.type memclear_, @function
+memclear_:
+    test rsi, rsi
+    jz .done_clear
+
+.loop_clear:
+    mov byte ptr [rdi], 0     # Clear byte-by-byte
+    inc rdi
+    dec rsi
+    jnz .loop_clear
+
+.done_clear:
     ret
